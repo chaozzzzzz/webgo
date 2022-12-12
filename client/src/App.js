cbe772e226632fcee3b952f7189d5a0432310172
wenhuang1 <huangwen@login.khoury.neu.edu>
2022-12-12 18:58:01 -0500

    Login page css

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Layout, Image, Typography } from "antd";
 import Logo from "./images/logo.png";
 import Home from './components/Home';
 import styles from './styles';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import { BrowserRouter, Route, Routes, Link, Navigate} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Link, Navigate } from 'react-router-dom'
 import Logout from './pages/Logout';
 import { Button } from "react-bootstrap";
 import axios from 'axios'
 import StoryForm from './components/StoryForm/StoryForm';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const { Title } = Typography;
 const { Header, Footer } = Layout;
@@ -29,16 +30,16 @@ const App = () => {
     return (
         <Layout style={styles.layout}>
             <Header style={styles.header}>
-                <Image  style={styles.image} preview={false} src={Logo} width={45} />
+                <Image style={styles.image} preview={false} src={Logo} width={45} />
                 &nbsp;
-                <Title  style={styles.title}>Webgo</Title>
+                <Title style={styles.title}>Webgo</Title>
 
-            {/* <div>
+                {/* <div>
                 <Button variant="danger" onClick={logoutClick}>Logout</Button>
             </div> */}
-      </Header>
+            </Header>
 
-            
+
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
@@ -48,7 +49,7 @@ const App = () => {
                     <Route path="/logout" element={<Logout />} />
                 </Routes>
             </BrowserRouter >
-            <Footer  style={styles.footer}>2022 Webgo</Footer>
+            <Footer style={styles.footer}>2022 Webgo</Footer>
         </Layout>
     )
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,16 @@
-import React, {useState} from 'react'
-import "../Login.css";
+import React, { useState } from 'react'
+// import "../Login.css";
 import axios from 'axios';
-import {Navigate} from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Login = () => {
     const url = `${process.env.REACT_APP_API_BASE_URL}/user/login`
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('')
-    const [redirect, setRedirect] =  useState(false)
-    const [register, setRegister] =  useState(false)
+    const [redirect, setRedirect] = useState(false)
+    const [register, setRegister] = useState(false)
 
     const submit = async (e) => {
         e.preventDefault();
@@ -21,22 +23,22 @@ const Login = () => {
         setRedirect(true);
     }
 
-    if(redirect) {
-        return <Navigate to={'/'}/>;
+    if (redirect) {
+        return <Navigate to={'/'} />;
     }
-    if(register) {
-        return <Navigate to={'/register'}/>;
+    if (register) {
+        return <Navigate to={'/register'} />;
     }
 
     return (
-        
+
         <main className="form-signin">
-            <form onSubmit={submit}>
+            {/* <form onSubmit={submit}>
                 <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
                 <div className="form-floating">
                     <input type="email" className="form-control" id="floatingInput" placeholder="name@exmaple.com"
-                            onChange={e => setEmail(e.target.value)}
+                        onChange={e => setEmail(e.target.value)}
                     />
                     <label htmlFor="floatingInput">Email address</label>
                 </div>
@@ -48,11 +50,92 @@ const Login = () => {
                 </div>
 
                 <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={()=>{
+                <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={() => {
                     setRegister(true)
                 }}>if new, Register here</button>
-            </form>
-        </main>           
+            </form> */}
+
+            <Form>
+                <div className="form-floating">
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label className="h3 mb-3 fw-normal">Please sign in</Form.Label>
+
+
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+                </div>
+
+
+                <div className="form-floating">
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                </div>
+
+                <div className='btn-group'>
+                    <Button className="btn btn-primary" variant="primary" type="submit">
+                        Sign In
+                    </Button>
+                    <Button className="btn btn-default" variant="primary" type="submit">
+                        Resigter
+                    </Button>
+                </div>
+            </Form>
+        </main>
+
+
+        // <Form>
+        //     <Form.Group className="mb-3" controlId="formBasicEmail">
+        //         <Form.Label>Please sign in</Form.Label>
+        //         <Form.Control type="email" placeholder="Enter email" />
+        //         <Form.Text className="text-muted">
+        //             We'll never share your email with anyone else.
+        //         </Form.Text>
+        //     </Form.Group>
+
+        //     <Form.Group className="mb-3" controlId="formBasicPassword">
+        //         <Form.Label>Password</Form.Label>
+        //         <Form.Control type="password" placeholder="Password" />
+        //     </Form.Group>
+
+        //     <Button variant="primary" type="submit">
+        //         Sign In
+        //     </Button>
+        //     <Button variant="primary" type="submit">
+        //         Resigter
+        //     </Button>
+        // </Form>
+
+
+        // <main>
+        //     <Form>
+        //         <Form.Group className="mb-3" controlId="formBasicEmail">
+        //             <Form.Label>Email address</Form.Label>
+        //             <Form.Control type="email" placeholder="Enter email" />
+        //             <Form.Text className="text-muted">
+        //                 We'll never share your email with anyone else.
+        //             </Form.Text>
+        //         </Form.Group>
+
+        //         <Form.Group className="mb-3" controlId="formBasicPassword">
+        //             <Form.Label>Password</Form.Label>
+        //             <Form.Control type="password" placeholder="Password" />
+        //         </Form.Group>
+        //         <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        //             <Form.Check type="checkbox" label="Check me out" />
+        //         </Form.Group>
+        //         <Button variant="primary" type="submit">
+        //             Submit
+        //         </Button>
+        //     </Form>
+        // </main>
+
+
+
     );
 };
 
